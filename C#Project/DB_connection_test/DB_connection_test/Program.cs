7d4c58e2ad360f3a52396235af68fd8ee0248fb2
Kim Jin <jinistory123@gmail.com>
2023-01-09 16:24:11 +0900

    add c# project

---
+++ C#Project/DB_connection_test/DB_connection_test/Program.cs
@@ -0,0 +1,62 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DB_connection_test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var conn = new NpgsqlConnection("Host=localhost;Username=postgres;Password=1234;Database=study"))
+            {
+                try
+                {
+                    conn.Open();
+                    using(var cmd = new NpgsqlCommand())
+                    {
+                        cmd.Connection= conn;
+                        cmd.CommandText = "select * from car";
+
+                        using (var reader = cmd.ExecuteReader())
+                        {
+                            Console.WriteLine("table column = {0}개", reader.FieldCount);
+
+                            while(reader.Read())
+                            {
+                                var data = new string[]
+                                {
+                                    reader["name"].ToString(),
+                                    reader["number"].ToString(),
+                                    reader["date"].ToString()
+                                };
+
+                                foreach(var x in data)
+                                {
+                                    Console.Write(x);
+                                    Console.Write(" -- ");
+                                }
+                                Console.WriteLine();
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("============== Error ==============");
+                    Console.WriteLine(ex.Message);
+                }
+
+                Console.ReadLine();
+            }
+        }
+
+        void connectionDB()
+        {
+
+        }
+    }
+}
---
+++ C#Project/Database_Selete_Delete_Client/Database_Selete_Delete_Client/Form1.cs
@@ -0,0 +1,111 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Database_Selete_Delete_Client
+{
+    public partial class Form1 : Form
+    {
+        Boolean isSuccess = false;
+        String commandText = "";
+        int connMode;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.bt_book.Click += new System.EventHandler(this.bt_book_Click);
+            this.bt_car.Click += new System.EventHandler(this.bt_car_Click);
+            this.bt_delete.Click += new System.EventHandler(this.bt_delete_Click);
+        }
+
+        void set_Column()
+        {
+            this.ch_name.Text = string.Format("Name");
+            this.ch_date.Text = string.Format("Date");
+        }
+
+        void DB_connection()
+        {
+            using (var conn = new NpgsqlConnection("Host=localhost;Username=postgres;Password=1234;Database=study"))
+            {
+                try
+                {
+                    conn.Open();
+                    using (var cmd = new NpgsqlCommand())
+                    {
+                        cmd.Connection = conn;
+                        cmd.CommandText = commandText;
+
+                        using (var reader = cmd.ExecuteReader())
+                        {
+                            ListViewItem item;
+
+                            while (reader.Read())
+                            {
+                                DateTime date = (DateTime)reader["date"];
+                                if(connMode == 0)
+                                {
+                                    string[] row = { reader["name"].ToString(), reader["author"].ToString(), date.ToString("yyyy-MM-dd") };
+                                    item = new ListViewItem(row);
+                                    listView1.Items.Add(item);
+                                }
+                                else
+                                {
+                                    string[] row = { reader["name"].ToString(), reader["number"].ToString(), date.ToString("yyyy-MM-dd") };
+                                    item = new ListViewItem(row);
+                                    listView1.Items.Add(item);
+                                }
+                            }
+                            isSuccess = true;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("============== Error ==============");
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        private void bt_book_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            set_Column();
+            connMode = 0;
+            commandText = "select * from book";
+            DB_connection();
+            this.ch_author.Text = string.Format("Author");
+        }
+
+        private void bt_car_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            set_Column();
+            connMode = 1;
+            commandText = "select * from car";
+            DB_connection();
+            this.ch_author.Text = string.Format("Number");
+        }
+
+        private void bt_delete_Click(object sender, EventArgs e)
+        {
+            if(isSuccess == true)
+            {
+
+            }
+            else
+            {
+                Console.WriteLine("No Connection");
+                return;
+            }
+        }
+    }
+}
